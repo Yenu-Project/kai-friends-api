b60ff0468b3b1e46bb519754829d0ade9e7761c4
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-03-13 22:49:47 +0900

    feat(api root): add api/ to api root

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+import app.views.user
 from app import views
 
 # create a router and register our viewsets with it
 router = DefaultRouter()
 router.register(r'userprofiles', views.UserProfileViewSet)
-router.register(r'users', views.UserViewSet)
+router.register(r'users', app.views.user.UserViewSet)
 
 # the API URLs are now determined automatically by the router
 urlpatterns = [
-    path('', include(router.urls))
+    path('api/', include(router.urls))
 ]
