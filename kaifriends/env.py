bebcdd8d5747445ffd466a39467a0ab09185e0c0
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-03-20 23:18:50 +0900

    fix(key): add new SECRET_KEY to django settings

--- kaifriends/env.py
+++ kaifriends/env.py
@@ -7,4 +7,5 @@ env = environ.Env(
     KAIFRIENDS_DB_PASSWORD=(str, os_environ.get('KAIFRIENDS_DB_PASSWORD', '')),
     KAIFRIENDS_DB_HOST=(str, os_environ.get('KAIFRIENDS_DB_HOST', 'localhost')),
     KAIFRIENDS_DB_PORT=(str, os_environ.get('KAIFRIENDS_DB_PORT', '3306')),
+    SECRET_KEY=(str, os_environ.get('SECRET_KEY', 'secret'))
 )
