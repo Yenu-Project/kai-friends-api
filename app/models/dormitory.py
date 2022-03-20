00ebfe3509629e107846263fb1f749be77daa1a8
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-03-20 22:31:50 +0900

    feat(dorm): add model for dormitory

---
+++ app/models/dormitory.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Dormitory(models.Model):
+    dorm_en = models.CharField(max_length=64, default='')
+    dorm_ko = models.CharField(max_length=64, default='')
+    boy = models.BooleanField(default=False)
+    girl = models.BooleanField(default=False)  # 남녀 공동 기숙사 존재 (세종관)
+    undergraduate = models.BooleanField(default=False)
+    graduate = models.BooleanField(default=False)
