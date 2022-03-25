d743c656a7e30df812e570c10e2f26eae8340dfe
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-03-25 11:26:10 +0900

    feat(profile): add profile model

--- app/models/user_profile.py
+++ app/models/user_profile.py
@@ -1,8 +1,55 @@
 from django.db import models
+from django.conf import settings
 
 
 # TODO: need to replace with Django auth user
 class UserProfile(models.Model):
-    user = models.ForeignKey('auth.User', related_name='userprofiles', on_delete=models.CASCADE)
-    username = models.CharField(max_length=128, default='')
-    email = models.CharField(max_length=128, default='')
+    user = models.OneToOneField(
+        to=settings.AUTH_USER_MODEL,
+        db_index=True,
+        related_name='profile',
+        on_delete=models.CASCADE,
+        verbose_name='사용자',
+    )
+    gender = models.BooleanField(
+        default=None,
+        verbose_name='성별'
+    )
+    age = models.PositiveSmallIntegerField(
+        default=0,
+        verbose_name='나이'
+    )
+    # TODO
+    # major =
+    degree = models.ForeignKey(
+        to='app.Degree',
+        default=None,
+        verbose_name='학위과정',
+        on_delete=models.CASCADE,
+    )
+    dormitory = models.ForeignKey(
+        to='app.Dormitory',
+        default=None,
+        verbose_name='기숙사',
+        on_delete=models.CASCADE,
+    )
+    # TODO: enable s3 storage and uncomment image
+    # image = models.ImageField(
+    #     null=True,
+    #     blank=True,
+    #     default=None,
+    #     upload_to='TODO',
+    #     verbose_name='프로필 이미지'
+    # )
+    introduction = models.CharField(
+        blank=True,
+        default='',
+        max_length=256,
+        verbose_name='자기소개'
+    )
+    kakao_url = models.CharField(
+        blank=True,
+        default='',
+        max_length=256,
+        verbose_name='카카오 오픈채팅 링크'
+    )
