f7b7fea75699e9767ee0e43cb6e9f5abed7656d3
kjy2844 <kjy2844@kaist.ac.kr>
2022-03-28 00:16:28 +0900

    feat(major): add model for major

---
+++ app/models/major.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Major(models.Model):
+    name_en = models.CharField(max_length=64, default='')
+    name_ko = models.CharField(max_length=64, default='')
+    abbreviation_ko = models.CharField(max_length=64, default='')
+    acronym_en = models.CharField(max_length=16, default='')
+
