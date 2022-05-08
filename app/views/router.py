61e5f6642c3f42b1184457fdc80fcc813b499807
kjy2844 <kjy2844@kaist.ac.kr>
2022-05-09 01:06:48 +0900

    feature(matching): Add mathing friends list API

--- app/views/router.py
+++ app/views/router.py
@@ -1,15 +1,24 @@
 from rest_framework import routers
 
 from app.views.viewsets import UserViewSet, UserProfileViewSet
+from app.views.viewsets.matching import MatchingViewSet
 
 router = routers.DefaultRouter()
 
 router.register(
     prefix=r'users',
     viewset=UserViewSet,
+    basename='users',
 )
 
 router.register(
     prefix=r'user_profiles',
     viewset=UserProfileViewSet,
+    basename='user_profiles',
+)
+
+router.register(
+    prefix=r'matching',
+    viewset=MatchingViewSet,
+    basename='matching',
 )
