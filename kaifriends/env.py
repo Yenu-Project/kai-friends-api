b6e1ec63b39a2aac8fe649667397d7a89d9531ef
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-05-13 14:51:41 +0900

    refactor(settings): remove default settings

--- kaifriends/env.py
+++ kaifriends/env.py
@@ -2,10 +2,10 @@ import environ
 from os import environ as os_environ
 
 env = environ.Env(
-    KAIFRIENDS_DB_NAME=(str, os_environ.get('KAIFRIENDS_DB_NAME', 'kai_friends')),
-    KAIFRIENDS_DB_USER=(str, os_environ.get('KAIFRIENDS_DB_USER', 'root')),
-    KAIFRIENDS_DB_PASSWORD=(str, os_environ.get('KAIFRIENDS_DB_PASSWORD', '')),
-    KAIFRIENDS_DB_HOST=(str, os_environ.get('KAIFRIENDS_DB_HOST', 'localhost')),
-    KAIFRIENDS_DB_PORT=(str, os_environ.get('KAIFRIENDS_DB_PORT', '3306')),
-    SECRET_KEY=(str, os_environ.get('SECRET_KEY', 'secret'))
+    KAIFRIENDS_DB_NAME=(str, os_environ.get('KAIFRIENDS_DB_NAME')),
+    KAIFRIENDS_DB_USER=(str, os_environ.get('KAIFRIENDS_DB_USER')),
+    KAIFRIENDS_DB_PASSWORD=(str, os_environ.get('KAIFRIENDS_DB_PASSWORD')),
+    KAIFRIENDS_DB_HOST=(str, os_environ.get('KAIFRIENDS_DB_HOST')),
+    KAIFRIENDS_DB_PORT=(str, os_environ.get('KAIFRIENDS_DB_PORT')),
+    SECRET_KEY=(str, os_environ.get('SECRET_KEY'))
 )
